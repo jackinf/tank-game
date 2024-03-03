619d82dee403880c6f6d3d03973db7f7f9f8d8e6
Jevgeni Rumjantsev <zeka.rum@gmail.com>
2024-03-03 14:45:36 +0100

    formatting

--- apps/ball-game/src/main.rs
+++ apps/ball-game/src/main.rs
@@ -3,9 +3,6 @@ mod ball_game;
 use bevy::prelude::*;
 
 #[derive(Component)]
-struct Position { x: f32, y: f32 }
-
-#[derive(Component)]
 struct Person;
 
 #[derive(Component)]
@@ -15,8 +12,7 @@ pub struct HelloPlugin;
 
 impl Plugin for HelloPlugin {
     fn build(&self, app: &mut App) {
-        app
-            .insert_resource(GreetTimer(Timer::from_seconds(2.0, TimerMode::Repeating)))
+        app.insert_resource(GreetTimer(Timer::from_seconds(2.0, TimerMode::Repeating)))
             .add_systems(Startup, add_people)
             .add_systems(Update, (greet_people, update_people).chain());
     }
@@ -24,9 +20,6 @@ impl Plugin for HelloPlugin {
 
 fn main() {
     ball_game::main();
-    // App::new()
-    //     .add_plugins((DefaultPlugins, HelloPlugin))
-    //     .run();
 }
 
 fn add_people(mut commands: Commands) {
@@ -47,23 +40,12 @@ fn update_people(mut query: Query<&mut Name, With<Person>>) {
 #[derive(Resource)]
 struct GreetTimer(Timer);
 
-fn greet_people(
-    time: Res<Time>,
-    mut timer: ResMut<GreetTimer>,
-    query: Query<&Name, With<Person>>
-) {
+fn greet_people(time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<&Name, With<Person>>) {
     // update our timer with the time elapsed since the last update
     // if that caused the timer to finish, we say hello to everyone
     if timer.0.tick(time.delta()).just_finished() {
         for name in &query {
             println!("hello {}!", name.0);
         }
-    }
-}
-
-
-fn print_position_system(query: Query<&Position>) {
-    for position in &query {
-        println!("position: {} {}", position.x, position.y);
     }
 }
