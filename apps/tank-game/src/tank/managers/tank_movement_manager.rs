1aaa78acee7c8d9b3bdf47867ae444dd92c914a3
Jevgeni Rumjantsev <zeka.rum@gmail.com>
2024-04-13 11:44:05 +0200

    move to target

--- apps/tank-game/src/tank/managers/tank_movement_manager.rs
+++ apps/tank-game/src/tank/managers/tank_movement_manager.rs
@@ -1,4 +1,5 @@
 use crate::common::components::tile::Tile;
+use crate::common::components::unit_id::UnitId;
 use crate::common::resources::game_map::GameMap;
 use crate::common::resources::me::Me;
 use bevy::input::keyboard::KeyboardInput;
@@ -51,50 +52,26 @@ impl TankMovementManager {
                 let wx = my_world_coords.0.x;
                 let wy = my_world_coords.0.y;
 
-                let clicked_on_tank = tank_query
+                let clicked_on_enemy_tank_id: Option<UnitId> = tank_query
                     .iter_mut()
-                    .find(|(tank, _)| tank.is_clicked_on(wx, wy));
+                    .find(|(tank, _)| tank.is_clicked_on(wx, wy) && !tank.is_mine(&me))
+                    .map(|(tank, _)| tank.get_id().clone());
 
-                match clicked_on_tank {
-                    Some((tank, _)) => {
-                        if !tank.is_mine(&me) {
-                            dbg!("not mine tank was clicked");
-                            let enemy_tank_id = tank.get_id().clone(); // clicked on enemy tank, try to attack it
-                            tank_query
-                                .iter_mut()
-                                .filter(|(tank, _)| tank.is_mine(&me) && tank.selected)
-                                .map(|(tank, _)| tank)
-                                .for_each(|mut my_selected_tank| {
-                                    dbg!("attacking enemy tank");
-                                    my_selected_tank.set_target(enemy_tank_id.clone());
-                                    // refer to tank_shooting_manager.rs
-                                });
-                        }
-                    }
-                    None => {
-                        if let Some(goal) =
-                            TileQueries::find_accessible(&tile_query, &my_world_coords.0)
+                if let Some(goal) = TileQueries::find_accessible(&tile_query, &my_world_coords.0) {
+                    let selected_tanks = &mut tank_query
+                        .iter_mut()
+                        .filter(|(tank, _)| tank.selected)
+                        .map(|(tank, _)| tank);
+
+                    for mut tank in selected_tanks {
+                        if let Some(start) =
+                            // TODO: why not use translation instead of target_position?
+                            TileQueries::find_accessible(&tile_query, &tank.target_position)
                         {
-                            let selected_tanks = &mut tank_query
-                                .iter_mut()
-                                .filter(|(tank, _)| tank.selected)
-                                .map(|(tank, _)| tank);
-
-                            for mut tank in selected_tanks {
-                                if let Some(start) =
-                                    // TODO: why not use translation instead of target_position?
-                                    TileQueries::find_accessible(
-                                        &tile_query,
-                                        &tank.target_position,
-                                    )
-                                {
-                                    // TODO: expensive! optimize this
-                                    // TODO: consider using use bevy::utils::petgraph::algo::astar;
-                                    let path_f32: VecDeque<(f32, f32)> = astar::find_path(
-                                        &game_map.get_tile_type_grid(),
-                                        start,
-                                        goal,
-                                    )
+                            // TODO: expensive! optimize this
+                            // TODO: consider using use bevy::utils::petgraph::algo::astar;
+                            let path_f32: VecDeque<(f32, f32)> =
+                                astar::find_path(&game_map.get_tile_type_grid(), start, goal)
                                     .iter()
                                     .filter_map(|&key| {
                                         game_map.get_tile_to_world_coordinates().get(&key)
@@ -102,10 +79,25 @@ impl TankMovementManager {
                                     .cloned() // Clone the (f32, f32) values to move them into the Vec
                                     .collect();
 
-                                    tank.set_movement_path(path_f32);
-                                }
-                            }
+                            tank.set_movement_path(path_f32);
+
+                            // if let Some(enemy_tank_id) = clicked_on_enemy_tank_id {
+                            //     dbg!("attacking enemy tank");
+                            //     tank.set_target(enemy_tank_id.clone());
+                            // }
                         }
+                    }
+
+                    if let Some(enemy_tank_id) = clicked_on_enemy_tank_id {
+                        tank_query
+                            .iter_mut()
+                            .filter(|(tank, _)| tank.is_mine(&me) && tank.selected)
+                            .map(|(tank, _)| tank)
+                            .for_each(|mut my_selected_tank| {
+                                dbg!("attacking enemy tank");
+                                my_selected_tank.set_target(enemy_tank_id.clone());
+                                // refer to tank_shooting_manager.rs
+                            });
                     }
                 }
             }
@@ -117,28 +109,54 @@ impl TankMovementManager {
         mut tank_query: Query<(&mut Transform, &mut Tank), (With<Tank>, Without<TankGun>)>,
         mut gun_query: Query<(&mut Transform, &TankGun), (With<TankGun>, Without<Tank>)>,
     ) {
+        let dt = time.delta_seconds();
+
         for (mut transform, mut tank) in tank_query.iter_mut().filter(|(_, tank)| tank.is_moving())
         {
             let current_pos = transform.translation.xy();
             let direction = tank.target_position - current_pos;
-            let distance_to_move = tank.speed * time.delta_seconds();
+            let distance_to_move = tank.speed * dt;
+
+            // TODO: improve code, and move into tank component
+            // let total_distance = (tank.target_position - current_pos).length();
+            let total_distance = if tank.movement_path.len() > 0 {
+                let xy = tank.movement_path.iter().last().unwrap().clone();
+                let vector = Vec2::new(xy.0, xy.1) - transform.translation.truncate();
+                let total_distance = vector.length();
+                dbg!(total_distance);
+                total_distance
+            } else {
+                let vector = tank.target_position - transform.translation.truncate();
+                let total_distance = vector.length();
+                dbg!(total_distance);
+                dbg!(total_distance);
+                total_distance
+            };
+            if total_distance < 50.0 {
+                tank.stop();
+                continue;
+            }
 
             // Smooth movement
             if direction.length() > distance_to_move {
                 let new_pos = current_pos + direction.normalize() * distance_to_move;
-                let target_vec3 = Vec3::new(new_pos.x, new_pos.y, transform.translation.z);
+                // let target_vec3 = Vec3::new(new_pos.x, new_pos.y, transform.translation.z);
 
                 // TODO: account for a bug if the speed is too high.
                 // if so, use simpler:
-                // transform.translation = Vec3::new(new_pos.x, new_pos.y, transform.translation.z);
-                transform.translation = transform
-                    .translation
-                    .lerp(target_vec3, tank.speed / 10.0 * time.delta_seconds());
+                transform.translation = Vec3::new(new_pos.x, new_pos.y, transform.translation.z);
+                // transform.translation = transform
+                //     .translation
+                //     .lerp(target_vec3, tank.speed / 10.0 * time.delta_seconds());
             } else {
                 transform.translation.x = tank.target_position.x;
                 transform.translation.y = tank.target_position.y;
 
-                tank.try_take_next_position_in_path();
+                if tank.has_target() && distance_to_move < 10.0 {
+                    tank.stop();
+                } else {
+                    tank.try_take_next_position_in_path();
+                }
             }
 
             // Rotate tank gun smoothly
