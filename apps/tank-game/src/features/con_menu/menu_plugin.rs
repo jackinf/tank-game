3a9bb8820b345566837e4a329d1e48fce47a9b09
Jevgeni Rumjantsev <zeka.rum@gmail.com>
2024-06-23 13:23:41 +0200

    move some apps to examples

--- apps/tank-game/src/features/con_menu/menu_plugin.rs
+++ apps/tank-game/src/features/con_menu/menu_plugin.rs
@@ -9,8 +9,6 @@ pub struct MenuPlugin;
 impl Plugin for MenuPlugin {
     fn build(&self, app: &mut App) {
         app.add_systems(PreStartup, setup)
-            // Only run the app when there is user input. This will significantly reduce CPU/GPU use.
-            .insert_resource(WinitSettings::desktop_app())
             .add_systems(Update, detect_mouse_over_container)
             .add_systems(Update, update_money_text)
             .add_systems(Update, update_power_text)
